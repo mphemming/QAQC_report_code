ad2b037ff2759502c840264e41557dcc3fd3144e
Michael Hemming <m.hemming@unsw.edu.au>
2020-05-29 16:18:35 +1000

    Javascript to grab text from file (currently not working)

---
+++ Code/Deployment.js
@@ -0,0 +1,4 @@
+var fs = require("fs");
+var text = fs.readFileSync("/Users/Michael/Documents/Work/UNSW/Work/QC_reports/text_files/Deployment.txt");
+var textByLine = text.split("\n")
+document.write('text');
