bbb86f276d83a223df8da4b68a0f60ac8596c084
Michael Hemming <m.hemming@unsw.edu.au>
2020-06-11 15:09:12 +1000

    New function 'select_keys' to slice dictionaries, now get times in/out water from netCDF file

--- Code/QCreport_netCDF.py
+++ Code/QCreport_netCDF.py
@@ -16,45 +16,59 @@ import QCreport_paths as paths
 import netCDF4 as nc
 import glob
 
+# %% -----------------------------------------------------------------------------------------------
+# Function to select mutiple dictionary keys
+
+def select_keys(dict,key_n):
+    if key_n > 0:
+        selected_keys = (range(0,key_n))
+        subdict = {x: dict[x] for x in selected_keys if x in dict}
+    else:
+        subdict = dict[key_n]
+    return subdict
 
 # %% -----------------------------------------------------------------------------------------------
 # Get attributes
+#
+abstract = {}
+author = {}
+author_email = {}
+comment = {}
+history = {}
+instrument = {}
+instrument_nominal_depth = {}
+instrument_sample_interval = {}
+instrument_serial_number = {}
+local_time_zone = {}
+platform_code = {}
+principal_investigator = {}
+quality_control_log = {}
+time_coverage_start = {}
+time_coverage_end = {}
+toolbox_input_file = {}
+toolbox_version = {}
+geospatial_lat_max = {}
+geospatial_lat_min = {}
+geospatial_lon_max = {}
+geospatial_lon_min = {}
+variables = {}
+var_names = {}
+time_units = {}
+time_comment = {}
+depth_comment = {}
+in_water = {}
+out_water = {}
 
-def get_netCDF(path):
+def get_netCDF(path):   
+ 
     # Get list of netCDF files in folder
     nc_files = glob.glob(path + "*" + paths.site_name + "*" + paths.deployment_file_date_identifier + "*.nc")
-    
+    len_files = len(nc_files)
+
     # Get netCDF attributes for each file
-    for file in range(len(nc_files)):
+    for file in range(len_files):
         f_info = nc.Dataset(nc_files[file])
-        
-        abstract = {}
-        author = {}
-        author_email = {}
-        comment = {}
-        history = {}
-        instrument = {}
-        instrument_nominal_depth = {}
-        instrument_sample_interval = {}
-        instrument_serial_number = {}
-        local_time_zone = {}
-        platform_code = {}
-        principal_investigator = {}
-        quality_control_log = {}
-        time_coverage_start = {}
-        time_coverage_end = {}
-        toolbox_input_file = {}
-        toolbox_version = {}
-        geospatial_lat_max = {}
-        geospatial_lat_min = {}
-        geospatial_lon_max = {}
-        geospatial_lon_min = {}
-        variables = {}
-        var_names = {}
-        time_units = {}
-        time_comment = {}
-        depth_comment = {}
-        
+
         if hasattr(f_info,'abstract'):
             abstract[file] = f_info.abstract
         else:
@@ -175,42 +189,51 @@ def get_netCDF(path):
         if 'DEPTH' in var_names[0]:
           Datt = f_info.variables['DEPTH']      
           depth_comment[file] = Datt.comment
+          
+        # get times in/out water
+        if hasattr(f_info,'quality_control_log'):
+            if 'imosInOutWaterQC' in f_info.quality_control_log:
+                f_string = str(f_info.quality_control_log)
+                pos = f_string.find('imosInOutWaterQC')
+                in_water[file] = f_info.quality_control_log[pos+20:pos+37]
+                out_water[file] = f_info.quality_control_log[pos+43:pos+60]
+        
        
     # Save information as a class    
     class nc_attributes:
-        abstract = abstract  
-        author = author
-        author_email = author_email
-        comment = comment
-        history = history
-        instrument = instrument
-        instrument_nominal_depth = instrument_nominal_depth
-        instrument_serial_number = instrument_serial_number
-        local_time_zone = local_time_zone
-        platform_code = platform_code
-        principal_investigator = principal_investigator
-        quality_control_log = quality_control_log
-        time_coverage_start = time_coverage_start
-        time_coverage_end = time_coverage_end
-        toolbox_input_file = toolbox_input_file
-        toolbox_version = toolbox_version
-        geospatial_lat_min = geospatial_lat_min
-        geospatial_lat_max = geospatial_lat_max 
-        geospatial_lon_min = geospatial_lon_min  
-        geospatial_lon_max = geospatial_lon_max       
-        variables = variables
-        var_names = var_names
-        time_units = time_units
-        time_comment = time_comment
-        depth_comment = depth_comment
-        
+        abstract = select_keys(abstract,len_files)
+        author = select_keys(author,len_files)
+        author_email = select_keys(author_email,len_files)
+        comment = select_keys(comment,len_files)
+        history = select_keys(history,len_files)
+        instrument = select_keys(instrument,len_files)
+        instrument_nominal_depth = select_keys(instrument_nominal_depth,len_files)
+        instrument_serial_number = select_keys(instrument_serial_number,len_files)
+        local_time_zone = select_keys(local_time_zone,len_files)
+        platform_code = select_keys(platform_code,len_files)
+        principal_investigator = select_keys(principal_investigator,len_files)
+        quality_control_log = select_keys(quality_control_log,len_files)
+        time_coverage_start = select_keys(time_coverage_start,len_files)
+        time_coverage_end = select_keys(time_coverage_end,len_files)
+        toolbox_input_file = select_keys(toolbox_input_file,len_files)
+        toolbox_version = select_keys(toolbox_version,len_files)
+        geospatial_lat_min = select_keys(geospatial_lat_min,len_files)
+        geospatial_lat_max = select_keys(geospatial_lat_max,len_files)
+        geospatial_lon_min = select_keys(geospatial_lon_min,len_files) 
+        geospatial_lon_max = select_keys(geospatial_lon_max,len_files)      
+        variables = select_keys(variables,len_files)
+        var_names = select_keys(var_names,len_files)
+        time_units = select_keys(time_units,len_files)
+        time_comment = select_keys(time_comment,len_files)
+        depth_comment = select_keys(depth_comment,len_files)
+        in_water = select_keys(in_water,len_files)
+        out_water = select_keys(out_water,len_files)
+
     return nc_attributes
         
 
-# %% -----------------------------------------------------------------------------------------------
-# get percentage of data flagged
 
-# TBC later...
+    
 
 
 
