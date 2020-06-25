6113c7a26a22549222ad581321569f9f14024159
Michael Hemming <m.hemming@unsw.edu.au>
2020-06-25 12:16:36 +1000

    Major Update
    o Shift from working locally to on sci-maths-ocean
    o creation of main paths suitable for working on MAC or on-campus/linux
    o case and switch style code added to select paths depending on site name (e.g. 'BMP070')
    o folders with files created in IMOS/Moorings_report/Automatic_reports
    o added paths for additional sensor types: SBE37, CTD timeseries
    o changed report saving directory

--- Code/QCreport_paths.py
+++ Code/QCreport_paths.py
@@ -18,6 +18,22 @@
 # o   Modify the paths for your use
 # o   PLEASE ENSURE TO INCLUDE FINAL '/' IN YOUR PATHS
 
+# %% -----------------------------------------------------------------------------------------------
+# Import modules
+
+import os
+
+#------------------------------------------------------------
+# Information 
+#-------------
+
+# These are modules required to get current path information
+
+#------------------------------------------------------------
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 # %% -----------------------------------------------------------------------------------------------
 # Choose site, deployment, add name of report maker 
@@ -35,32 +51,149 @@ name_of_reportmaker = 'Michael Hemming'
 # __________________________________________________________________________________________________
 
 # %% -----------------------------------------------------------------------------------------------
+# Determine main paths
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+# get current directory
+pwd_info = os.getcwd()
+# depending on where working, create path
+if ('home' in pwd_info) == 0:
+    # working from MAC using mounted sci-maths-ocean
+    starting_path = '/Volumes/'
+else:
+    # Working from campus / ssh login
+    z = pwd_info.find('z')
+    zuser = pwd_info[z:z+8]
+    starting_path = '/home/' + zuser
+# remaining part of path
+ending_path = 'sci-maths-ocean/IMOS/Moorings_Report/Automatic_reporting/'
+ending_path_data = 'sci-maths-ocean/IMOS/DATA/MOORINGS/PROCESSED_2_5/'
+# check if missing '/' at end of strings
+if ('/' in starting_path[-1]) == 0:
+    starting_path = starting_path + '/'
+if ('/' in ending_path[-1]) == 0:
+    ending_path = ending_path + '/'    
+# complete path
+main_path = starting_path + ending_path
+main_path_data = starting_path + ending_path_data
+
+#------------------------------------------------------------
+# Information 
+#-------------
+        
+# Determine where script is being run from (MAC or server). 
+# Does not work for windows for the time being. Gets
+# the main path for folder containing code etc. and data
+
+#------------------------------------------------------------
+
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+# %% -----------------------------------------------------------------------------------------------
 # netCDF files
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
 
-if 'BMP070' in site_name:
+# Functions to choose correct paths to netCDF files
+#------------------------------------------------------------
+def ncdir_TEMP(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/TEMPERATURE/',
+            'BMP120': main_path_data + 'BMP120/TEMPERATURE/',
+            'CH050': main_path_data + 'CH050/TEMPERATURE/',
+            'CH070': main_path_data + 'CH070/TEMPERATURE/',
+            'CH100': main_path_data + 'CH100/TEMPERATURE/',
+            'PH100': main_path_data + 'PH100/TEMPERATURE/',
+            'SYD100': main_path_data + 'SYD100/TEMPERATURE/',
+            'SYD140': main_path_data + 'SYD140/TEMPERATURE/',
+            'ORS065': main_path_data + 'ORS065/TEMPERATURE/',
+            }
 
-    def ncdir_TEMP():
-        netCDF_TEMP_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Example_data_BMP070_29/TEMPERATURE/'
-        return netCDF_TEMP_dir
-    
-    def ncdir_CURR():
-        netCDF_CURR_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Example_data_BMP070_29/CURRENT/'
-        return netCDF_CURR_dir
-    
-    def ncdir_BGC():
-        netCDF_BGC_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Example_data_BMP070_29/BGC/'
-        return netCDF_BGC_dir
+    netCDF_TEMP_dir = switcher.get(site_name)
+    return netCDF_TEMP_dir
+# Functions to choose correct paths to netCDF files
+#------------------------------------------------------------
+def ncdir_SBE37(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/SBE37/',
+            'BMP120': main_path_data + 'BMP120/SBE37/',
+            'CH050': main_path_data + 'CH050/SBE37/',
+            'CH070': main_path_data + 'CH070/SBE37/',
+            'CH100': main_path_data + 'CH100/SBE37/',
+            'PH100': main_path_data + 'PH100/SBE37/',
+            'SYD100': main_path_data + 'SYD100/SBE37/',
+            'SYD140': main_path_data + 'SYD140/SBE37/',
+            'ORS065': main_path_data + 'ORS065/SBE37/',
+            }
 
+    netCDF_SBE37_dir = switcher.get(site_name)
+    return netCDF_SBE37_dir
+#------------------------------------------------------------
+def ncdir_CURR(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/CURRENT/',
+            'BMP120': main_path_data + 'BMP120/CURRENT/',
+            'CH050': main_path_data + 'CH050/CURRENT/',
+            'CH070': main_path_data + 'CH070/CURRENT/',
+            'CH100': main_path_data + 'CH100/CURRENT/',
+            'PH100': main_path_data + 'PH100/CURRENT/',
+            'SYD100': main_path_data + 'SYD100/CURRENT/',
+            'SYD140': main_path_data + 'SYD140/CURRENT/',
+            'ORS065': main_path_data + 'ORS065/CURRENT/',
+            }
+
+    netCDF_CURR_dir = switcher.get(site_name)
+    return netCDF_CURR_dir
+#------------------------------------------------------------
+def ncdir_BGC(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/BGC/',
+            'BMP120': main_path_data + 'BMP120/BGC/',
+            'CH050': main_path_data + 'CH050/BGC/',
+            'CH070': main_path_data + 'CH070/BGC/',
+            'CH100': main_path_data + 'CH100/BGC/',
+            'PH100': main_path_data + 'PH100/BGC/',
+            'SYD100': main_path_data + 'SYD100/BGC/',
+            'SYD140': main_path_data + 'SYD140/BGC/',
+            'ORS065': main_path_data + 'ORS065/BGC/',
+            }
+
+    netCDF_BGC_dir = switcher.get(site_name)
+    return netCDF_BGC_dir
+#------------------------------------------------------------
+def ncdir_CTD(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/CTD/',
+            'BMP120': main_path_data + 'BMP120/CTD/',
+            'CH050': main_path_data + 'CH050/CTD/',
+            'CH070': main_path_data + 'CH070/CTD/',
+            'CH100': main_path_data + 'CH100/CTD/',
+            'PH100': main_path_data + 'PH100/CTD/',
+            'SYD100': main_path_data + 'SYD100/CTD/',
+            'SYD140': main_path_data + 'SYD140/CTD/',
+            'ORS065': main_path_data + 'ORS065/CTD/',
+            }
+
+    netCDF_CTD_dir = switcher.get(site_name)
+    return netCDF_CTD_dir
+#------------------------------------------------------------
+# get netCDF paths
+netCDF_TEMP_dir = ncdir_TEMP(site_name) 
+netCDF_SBE37_dir = ncdir_SBE37(site_name) 
+netCDF_CURR_dir = ncdir_CURR(site_name) 
+netCDF_BGC_dir = ncdir_BGC(site_name) 
+netCDF_CTD_dir = ncdir_CTD(site_name) 
 
 #------------------------------------------------------------
 # Information 
 #-------------
         
 # Select paths for site selected
-# Need to update this section when working from the server
 
 #------------------------------------------------------------
 
@@ -74,18 +207,33 @@ if 'BMP070' in site_name:
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
 
-if 'BMP070' in site_name:
+# Function to choose correct path to mooring diagram
+#------------------------------------------------------------
+def mddir(site_name):
+    switcher = {
+            'BMP070': main_path + 'Mooring_Diagrams/BMP070/BMP070_120_mooring_diagram.png',
+            'BMP120': main_path + 'Mooring_Diagrams/BMP120/BMP070_120_mooring_diagram.png',
+            'CH050': main_path + 'Mooring_Diagrams/CH050/',
+            'CH070': main_path + 'Mooring_Diagrams/CH070/',
+            'CH100': main_path + 'Mooring_Diagrams/CH100/',
+            'PH100': main_path + 'Mooring_Diagrams/PH100/',
+            'SYD100': main_path + 'Mooring_Diagrams/SYD100/',
+            'SYD140': main_path + 'Mooring_Diagrams/SYD140/',
+            'ORS065': main_path + 'Mooring_Diagrams/ORS065/',
+            }
 
-    def mddir():
-        md_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Example_data_BMP070_29/Mooring_Diagram/BMP070_120_mooring_diagram.png'
-        return md_dir
+    md_dir = switcher.get(site_name)
+    return md_dir
+
+#------------------------------------------------------------
+# get Mooring diagram path
+md_dir = mddir(site_name) 
 
 #------------------------------------------------------------
 # Information 
 #-------------
         
 # Select paths for site mooring diagram file
-# Need to update this section when working from the server
 
 #------------------------------------------------------------
 
@@ -99,18 +247,33 @@ if 'BMP070' in site_name:
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
 
-if 'BMP070' in site_name:
+# Function to choose correct path to Deployment photographs
+#------------------------------------------------------------
+def dppdir(site_name):
+    switcher = {
+            'BMP070': main_path + 'Deployment_Photographs/BMP070/Deployment_' + deployment + '/',
+            'BMP120': main_path + 'Deployment_Photographs/BMP120/Deployment_' + deployment + '/',
+            'CH050': main_path + 'Deployment_Photographs/CH050/Deployment_' + deployment + '/',
+            'CH070': main_path + 'Deployment_Photographs/CH070/Deployment_' + deployment + '/',
+            'CH100': main_path + 'Deployment_Photographs/CH100/Deployment_' + deployment + '/',
+            'PH100': main_path + 'Deployment_Photographs/PH100/Deployment_' + deployment + '/',
+            'SYD100': main_path + 'Deployment_Photographs/SYD100/Deployment_' + deployment + '/',
+            'SYD140': main_path + 'Deployment_Photographs/SYD140/Deployment_' + deployment + '/',
+            'ORS065': main_path + 'Deployment_Photographs/ORS065/Deployment_' + deployment + '/',
+            }
 
-    def dppdir():
-        dpp_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Example_data_BMP070_29/Deployment_photographs/'
-        return dpp_dir
+    dpp_dir = switcher.get(site_name)
+    return dpp_dir
+
+#------------------------------------------------------------
+# get Mooring diagram path
+dpp_dir = dppdir(site_name) 
     
 #------------------------------------------------------------
 # Information 
 #-------------
         
 # Select paths for deployment photographs
-# Need to update this section when working from the server
 
 #------------------------------------------------------------    
 
@@ -124,11 +287,27 @@ if 'BMP070' in site_name:
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
   
-if 'BMP070' in site_name:    
-    
-    def tbdir():
-        toolboxplots_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/Toolbox_Plots/'
-        return toolboxplots_dir
+# Function to choose correct path to Deployment photographs
+#------------------------------------------------------------
+def tbdir(site_name):
+    switcher = {
+            'BMP070': main_path_data + 'BMP070/PLOTS_TOOLBOX/',
+            'BMP120': main_path_data + 'BMP120/PLOTS_TOOLBOX/',
+            'ORS065': main_path_data + 'ORS065/PLOTS_TOOLBOX/',
+            'CH050': main_path_data + 'CH050/PLOTS_TOOLBOX/',
+            'CH070': main_path_data + 'CH070/PLOTS_TOOLBOX/',
+            'CH100': main_path_data + 'CH100/PLOTS_TOOLBOX/',
+            'PH100': main_path_data + 'PH100/PLOTS_TOOLBOX/',
+            'SYD100': main_path_data + 'SYD100/PLOTS_TOOLBOX/',
+            'SYD140': main_path_data + 'SYD140/PLOTS_TOOLBOX/',
+            }
+
+    tb_dir = switcher.get(site_name)
+    return tb_dir
+
+#------------------------------------------------------------
+# get Mooring diagram path
+tb_dir = tbdir(site_name) 
 
 #------------------------------------------------------------
 # Information 
@@ -150,7 +329,7 @@ if 'BMP070' in site_name:
 # __________________________________________________________________________________________________
 
 def savedir():
-    save_dir = '/Users/Michael/Documents/Work/UNSW/Work/QC_reports/'
+    save_dir = main_path + 'Reports/'
     return save_dir
 
 # __________________________________________________________________________________________________
