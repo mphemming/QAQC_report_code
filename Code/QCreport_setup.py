890352c110ea436855cc3eecc491bf7226e47e07
Michael Hemming <m.hemming@unsw.edu.au>
2020-06-29 16:48:25 +1000

    setup script
    o script used to setup the report
    o includes site and deployment information and QC comments

---
+++ Code/QCreport_setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Created on Mon Jun 29 16:11:18 2020
+# Contributers:  Michael Hemming (NSW-IMOS), Neil Malan (NSW-IMOS)
+# contact email: m.hemming@unsw.edu.au
+
+# %% -----------------------------------------------------------------------------------------------
+# Choose site, deployment, add name of report maker 
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+site_name = 'BMP070'
+deployment = '29'
+deployment_file_date_identifier = '1904'
+name_of_reportmaker = 'Michael Hemming'
+
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+
+# %% -----------------------------------------------------------------------------------------------
+# Add Comments to Report 
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+QCcomment = 'Some'
+# options: 'None', 'Some', 'Moderate', 'Substantial'
+QCmethod = 'Automatic and manual'
+# options: 'Automatic', 'Manual', 'Automatic and manual'
+Fieldwork_issues = 'None'
+# default: 'None', if issues add text here
+Sensor_damage = 'None'
+# default: 'None', if damage add text here
+Lost_equipment = 'None'
+# default: 'None', if lost equipment add text here
+biofouling = 'Some'
+# options: 'None', 'Some', 'Moderate', 'Substantial'
+Other_issues = 'None'
+# default: 'None', if other issues add text here
+Additional_QC = 'Yes'
+# options: 'yes' or 'no'
+comments = 'Some pressure sensor drift. See end of report for details.'
+# default: 'None', if comments add text here   
+# tips: copy & paste might not work - only use characters in 'latin-q' codec
+# e.g. commas copied over from somewhere else may be different to commas in latin-1
+# use '' instead       
+
+#------------------------------------------------------------
+# Information 
+#-------------
+        
+# Information added here will be included in the introduction 
+# comments at the start of the deployment report
+
+#------------------------------------------------------------               
+            
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+
+
