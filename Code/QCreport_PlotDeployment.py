cd05c769bad56ca27e2c60b8a819403b23984e3c
mphemming <mphemming@live.co.uk>
2023-01-25 15:53:03 +1100

    New script to create the plot deployment figure for inclusion in report

---
+++ Code/QCreport_PlotDeployment.py
@@ -0,0 +1,41 @@
+# Created on Wed Jan 25 09:39:46 2023
+# Contributers (code):  Michael Hemming (NSW-IMOS), Neil Malan (NSW-IMOS)
+# Contributers (review): Moninya Roughan (NSW-IMS), Tim Austin (NSW-IMOS), Stuart Milburn (NSW-IMOS) 
+# contact email: m.hemming@unsw.edu.au
+
+# %% -----------------------------------------------------------------------------------------------
+# Import packages
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________
+
+import xarray as xr
+import numpy as np
+import matplotlib
+matplotlib.use('qt5Agg')
+from matplotlib import pyplot as plt
+import datetime as dt
+import glob
+# QCreport modules
+import QCreport_paths as paths
+import QCreport_format as form
+import QCreport_DeploymentDetails as DepDet
+import QCreport_QualityControl as QCR
+import QCreport_DeploymentPhotographs as DepPhoto
+import QCreport_ToolboxPlots as tbp
+import QCreport_setup as setup
+import QCreport_cover as cover
+import QCreport_AdditionalPlots as Addp
+
+
+# plan
+# (1) create new gridded product that is every 1m depth for vel and temp (in other script)
+# (2) rotate velocity, depending on site
+# (3) Temp and vel plot over time for deployment
+
+
+# %% -----------------------------------------------------------------------------------------------
+# Required functions
+
+
+
