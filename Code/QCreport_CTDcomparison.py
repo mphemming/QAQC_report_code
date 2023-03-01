695d1239849fd81086ecdf10d76f7fe63fd05ed1
mphemming <mphemming@live.co.uk>
2023-03-01 17:18:46 +1100

    removed unnecessary imports

--- Code/QCreport_CTDcomparison.py
+++ Code/QCreport_CTDcomparison.py
@@ -21,15 +21,8 @@ import datetime as dt
 import glob
 # QCreport modules
 import QCreport_paths as paths
-import QCreport_format as form
 import QCreport_DeploymentDetails as DepDet
-import QCreport_QualityControl as QCR
-import QCreport_DeploymentPhotographs as DepPhoto
-import QCreport_ToolboxPlots as tbp
 import QCreport_setup as setup
-import QCreport_cover as cover
-import QCreport_AdditionalPlots as Addp
-
 
 # %% -----------------------------------------------------------------------------------------------
 # Required functions
