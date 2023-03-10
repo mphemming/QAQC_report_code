c79163f1be10172fdced854d8462200f2a9923a0
mphemming <mphemming@live.co.uk>
2023-03-10 16:43:07 +1100

    minor changes

--- Code/QCreport_setup.py
+++ Code/QCreport_setup.py
@@ -127,4 +127,13 @@ surname = name_of_reportmaker[findspace+1:]
 #     ' UNSW - Integrated Marine Observing System, Australia.'    
 # __________________________________________________________________________________________________
 # __________________________________________________________________________________________________
-# __________________________________________________________________________________________________
+# __________________________________________________________________________________________________site_name = 'BMP070'; 
+site_name = 'BMP070'; 
+deployment_file_date_identifier = '1411'; 
+print(site_name + '   |   ' + deployment_file_date_identifier); 
+site_name = 'BMP070'; 
+deployment_file_date_identifier = '1411'; 
+print(site_name + '   |   ' + deployment_file_date_identifier); 
+site_name = 'BMP070'; 
+deployment_file_date_identifier = '1411'; 
+print(site_name + '   |   ' + deployment_file_date_identifier); 
